8cffb68545bde6d008d35864486113f35f214be6
alibh97 <alibh97@users.noreply.github.com>
2022-12-16 17:12:03 +0330

    Si replaced by numbers, states plotd in time

--- main.py
+++ main.py
@@ -4,17 +4,7 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
-
-
-def observe():
-    if state == 'S1':
-        o = np.random.choice(['H', 'T'], p=B[0])
-    elif state == 'S2':
-        o = np.random.choice(['H', 'T'], p=B[1])
-    else:
-        o = np.random.choice(['H', 'T'], p=B[2])
-    return o
-
+# from scipy.stats import norm
 
 # Define the transition matrix
 A = np.array([[1/3, 1/3, 1/3],
@@ -35,91 +25,54 @@ n = 20
 states = []
 # empty observations sequense array
 observations = []
+# empty time array
+times = []
 
 # choose first state
-state = np.random.choice(['S1', 'S2', 'S3'], p=pi)
+state = np.random.choice([1, 2,3], p=pi)
 # append first state to states array
 states.append(state)
 
 # choose first observation
-observation=observe()
+observation = np.random.choice(['H', 'T'], p=B[state-1])
 
 # append first observation to observations array
 observations.append(observation)
 
-print('time',1,' State',state,' : ', observation)
+time=1
+# append first time to times array
+times.append(time)
+
+print('time',time,' State',state,' : ', observation)
 
 # loop over the number of tosses
 for i in range(n-1):
+    time=time+1
+    times.append(time)
     # choose next state
-    if state == 'S1':
-        state = np.random.choice(['S1', 'S2', 'S3'], p=A[0])
-    elif state == 'S2':
-        state = np.random.choice(['S1', 'S2', 'S3'], p=A[1])
-    else:
-        state = np.random.choice(['S1', 'S2', 'S3'], p=A[2])
+    state = np.random.choice([1, 2,3], p=A[state-1])
 
     # append next state to states array
     states.append(state)
 
     # choose next observation
-    observation=observe()
+    observation= np.random.choice(['H', 'T'], p=B[state-1])
 
     # append next observation to observations array
     observations.append(observation)
 
-    print('time',i+2,' State',state,' : ', observation)
+    print('time',time,' State',state,' : ', observation)
 
 
+# plot the states in time
+# numbers in the x and y axis not float
+plt.figure(figsize=(10, 5))
+plt.xticks(np.arange(0, n+1, 1))
+plt.yticks(np.arange(0, 4, 1))
+plt.plot(times, states, 'o-')
+plt.xlabel('time')
+plt.ylabel('states')
+plt.title('States in time')
+plt.show()
 
-# # Define the observations
-# observations = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-#
-# # Define the number of states
-# num_states = A.shape[0]
-# #
-# # Define the number of observations
-# num_observations = len(observations)
-#
-# # Define the number of iterations
-# num_iterations = 10
-#
-# # Define the number of frames
-# num_frames = num_iterations + 1
-#
-# # Define the figure
-# fig = plt.figure()
-#
-# # Define the axes
-# ax = fig.add_subplot(111)
-#
-# # Define the title
-# ax.set_title('Hidden Markov Model')
-#
-# # Define the x-axis
-# ax.set_xlabel('Observations')
-#
-# # Define the y-axis
-# ax.set_ylabel('States')
-#
-# # Define the x-axis limits
-# ax.set_xlim(0, num_observations)
-#
-# # Define the y-axis limits
-# ax.set_ylim(0, num_states)
-#
-# # Define the x-axis ticks
-# ax.set_xticks(range(num_observations))
-#
-# # Define the y-axis ticks
-# ax.set_yticks(range(num_states))
-#
-# # Define the x-axis tick labels
-# ax.set_xticklabels(observations)
-#
-# # Define the y-axis tick labels
-# ax.set_yticklabels(range(num_states))
-#
-#
-# fig.show()
 
