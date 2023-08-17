3db5638cfa6cee5ff6873071b5fe0c36439b8651
alibh97 <alibh97@users.noreply.github.com>
2023-08-17 19:37:59 +0330

    last change

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ from matplotlib import pyplot as plt
 # from scipy.stats import norm
 
 # Define the transition matrix
-# A = np.array([[1/4, 2/4, 1/4],
-#                 [2/5, 1/5, 2/5],
-#                 [2/6, 3/6, 1/6]])
+A = np.array([[1/4, 2/4, 1/4],
+                [1/5, 1/5, 3/5],
+                [1/6, 3/6, 2/6]])
 
-A = np.array([[1 / 3, 1 / 3, 1 / 3],
-              [1 / 3, 1 / 3, 1 / 3],
-              [1 / 3, 1 / 3, 1 / 3]])
+# A = np.array([[1 / 3, 1 / 3, 1 / 3],
+#               [1 / 3, 1 / 3, 1 / 3],
+#               [1 / 3, 1 / 3, 1 / 3]])
 
 # Define the emission matrix
 B = np.array([[0.5, 0.5],
-              [0.75, 0.25],
-              [0.25, 0.75]])
+              [0.9, 0.1],
+              [0.1, 0.9]])
 
 # Define the initial state distribution
 pi = np.array([1 / 3, 1 / 3, 1 / 3])
 
 # Define the number of tosses
-n = 20
+n = 100
 # empty states sequense array
 states = []
 # empty observations sequense array
@@ -48,7 +48,6 @@ time = 1
 # append first time to times array
 times.append(time)
 
-print('time', time, ' State', state, ' : ', observation)
 
 # loop over the number of tosses
 for i in range(n - 1):
@@ -66,7 +65,7 @@ for i in range(n - 1):
     # append next observation to observations array
     observations.append(observation)
 
-    print('time', time, ' State', state, ' : ', observation)
+    # print('time', time, ' State', state, ' : ', observation)
 
 # plot the states in time
 # numbers in the x and y axis not float
@@ -146,7 +145,7 @@ for i in range(len(states)):
         rate = rate + 1
 
 rate = rate * 100 / len(states)
-print('rate of similarity between the most probable states and the real states is : ', rate)
+print('similarity rate between q and the real states is : ', rate)
 
 # plot states and most probable states in time in one plot with different colors
 # numbers in the x and y axis not float
@@ -157,7 +156,7 @@ plt.plot(times, states, 'o-', color='red', label='real states')
 plt.plot(times, most_probable_states, 'o-', color='blue', label='viterbi states')
 plt.xlabel('time')
 plt.ylabel('states')
-plt.title('States and most probable states in time')
+plt.title('Real States and most probable states in time')
 plt.legend()
 plt.show()
 
